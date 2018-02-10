71370a2e7e4ad5fa032eff30fd5ff8e38e4c44d9
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-02-10 13:13:32 -0600

    learning

--- indecision/public/scripts/app.js
+++ indecision/public/scripts/app.js
@@ -1,1 +1,12 @@
 console.log('App.js is running!');
+
+// var template = <p>This is JSX from app.js!</p>;
+
+var template = React.createElement(
+    "h1",
+    { id: "someid" },
+    "Something new"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
