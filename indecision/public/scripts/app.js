8fbe6fbcb066d757b1374f44e55a2bf648269507
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-02-12 16:36:58 -0600

    jsx expressions

--- indecision/public/scripts/app.js
+++ indecision/public/scripts/app.js
@@ -2,11 +2,62 @@
 
 console.log('App.js is running!');
 
-// JSX - Javascript XML
+var app = {
+	title: 'Death Row Records',
+	subtitle: 'Los Angeles'
+};
 var template = React.createElement(
-  'p',
-  null,
-  'Does this change?'
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		app.title
+	),
+	React.createElement(
+		'p',
+		null,
+		app.subtitle
+	),
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
+			null,
+			'Item one'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Item two'
+		)
+	)
+);
+
+var userName = 'Tupac';
+var userAge = 28;
+var userLocation = 'Italy';
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		userName.toUpperCase()
+	),
+	React.createElement(
+		'p',
+		null,
+		'Age: ',
+		userAge
+	),
+	React.createElement(
+		'p',
+		null,
+		'Location: ',
+		userLocation
+	)
 );
 var appRoot = document.getElementById('app');
 
--- indecision/src/app.js
+++ indecision/src/app.js
@@ -1,7 +1,34 @@
 console.log('App.js is running!');
 
-// JSX - Javascript XML
-var template = <p>Does this change?</p>;
+
+var app = {
+	title: 'Death Row Records',
+	subtitle: 'Los Angeles'
+};
+var template = (
+<div>
+ <h1>{app.title}</h1>
+ <p>{app.subtitle}</p>
+ <ol>
+  <li>Item one</li>
+  <li>Item two</li>
+ </ol>
+</div>
+);
+
+ 
+
+
+var userName = 'Tupac';
+var userAge = 28
+var userLocation = 'Italy'
+var templateTwo = (
+	<div>
+	  <h1>{userName.toUpperCase()}</h1>
+	  <p>Age: {userAge}</p>
+	  <p>Location: {userLocation}</p>
+	 </div>
+	);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
