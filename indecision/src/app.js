7e08c15c94bb01e1f6c2201e165ac37636c04b68
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-03-01 17:18:03 -0600

    data

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -20,18 +20,33 @@ const template = (
 
 let count = 0;
 const addOne = () => {
-	console.log('addOne');
-}
-const templateTwo = (
- <div>
-  <h1>Count: {count}</h1>
-  <button onClick={addOne}>+1</button>
-</div>
-);
-
-
-
+	count++;
+	renderCounterApp();
+};
+const minusOne = () => {
+	count--;
+	renderCounterApp();
+};
+const reset = () => {
+	count = 0;
+	renderCounterApp();
+};
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+
+
+const renderCounterApp = () => {
+	const templateTwo = (
+ <div>
+  <h1>Count: {count}</h1>
+  <button onClick={addOne}>+1</button>
+  <button onClick={minusOne}>-1</button>
+  <button onClick={reset}>reset</button>
+</div>
+);
+
+ ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
