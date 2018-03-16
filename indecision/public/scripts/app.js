0835ac8160acc40a7a7b6f7afd90c64c44b3ef0c
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-03-16 01:54:53 -0500

    toggle

--- indecision/public/scripts/app.js
+++ indecision/public/scripts/app.js
@@ -1,88 +1,38 @@
 'use strict';
 
-console.log('App.js is running!');
+var visibility = false;
 
-var app = {
-  title: 'Death Row Records',
-  subtitle: 'Chicago is the best city in the world',
-  options: []
+var toggleVisibility = function toggleVisibility() {
+	visibility = !visibility;
+	render();
 };
-
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-
-  var option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    render();
-  }
-};
-var onRemoveAll = function onRemoveAll() {
-  app.options = [];
-  render();
-};
-var onMakeDecision = function onMakeDecision() {
-  var randoNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randoNum];
-  alert(option);
-};
-
-var appRoot = document.getElementById('app');
 
 var render = function render() {
-  var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      app.title
-    ),
-    app.subtitle && React.createElement(
-      'p',
-      null,
-      app.subtitle
-    ),
-    React.createElement(
-      'p',
-      null,
-      app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-      'button',
-      { disabled: app.options.length === 0, onClick: onMakeDecision },
-      'What should I do'
-    ),
-    React.createElement(
-      'button',
-      { onClick: onRemoveAll },
-      'Remove All'
-    ),
-    React.createElement(
-      'ol',
-      null,
-      app.options.map(function (option) {
-        return React.createElement(
-          'li',
-          { key: option },
-          option
-        );
-      })
-    ),
-    React.createElement(
-      'form',
-      { onSubmit: onFormSubmit },
-      React.createElement('input', { type: 'text', name: 'option' }),
-      React.createElement(
-        'button',
-        null,
-        'Add options'
-      )
-    )
-  );
-  ReactDOM.render(template, appRoot);
+	var jsx = React.createElement(
+		'div',
+		null,
+		React.createElement(
+			'h1',
+			null,
+			'Visibility Toggle'
+		),
+		React.createElement(
+			'button',
+			{ onClick: toggleVisibility },
+			visibility ? 'Hide details' : 'Show details'
+		),
+		visibility && React.createElement(
+			'div',
+			null,
+			React.createElement(
+				'p',
+				null,
+				'Hey. These are some things you can see!'
+			)
+		)
+	);
+
+	ReactDOM.render(jsx, document.getElementById('app'));
 };
 
 render();
