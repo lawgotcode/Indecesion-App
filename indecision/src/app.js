e01895ca7ea8fafa9f3bb1759c4a82a1e301147d
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-03-20 23:42:41 -0500

    react

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -1,62 +1,32 @@
-console.log('App.js is running!');
+ class Header extends React.Component {
+     render() {
+     	return (
+     		<div>
+     		 <h1>Indecision</h1>
+     		 <h2>Put your life in the hands of a computer</h2>
+     		</div>
+     		);
+     }
+ }
+
+ class Action extends React.Component {
+ 	render() {
+ 		return (
+ 		 <div>
+ 		  <button>What should I do</button>
+ 		 </div>
+ 		);
+ 	}
+ }
+
+ const jsx = (
+  <div>
+   <h1>Title</h1>
+   <Header />
+   <Action />
+   
+  </div>
+ );
 
 
-const app = {
-	title: 'Death Row Records',
-	subtitle: 'Chicago is the best city in the world',
-	options: []
-};
-
-const onFormSubmit = (e) => {
-	e.preventDefault();
-
-	const option = e.target.elements.option.value;
-
-	if (option) {
-		app.options.push(option);
-		e.target.elements.option.value = '';
-		render();
-	}
-  
-};
-const onRemoveAll = () => {
-	app.options = [];
-	render();
-};
-const onMakeDecision = () => {
-	const randoNum = Math.floor(Math.random() * app.options.length);
-	const option = app.options[randoNum];
-	alert(option);
-}
-
-const appRoot = document.getElementById('app');
-
-
-
-
-const render = () => {
-  const template = (
-<div>
- <h1>{app.title}</h1>
- {app.subtitle && <p>{app.subtitle}</p>}
- <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
- <button disabled={app.options.length === 0}onClick={onMakeDecision}>What should I do</button>
- <button onClick={onRemoveAll}>Remove All</button>
-<ol>
-  {
-  	app.options.map((option) => {
-  		return <li key={option}>{option}</li>
-  	})
-  }
- </ol>
- <form onSubmit={onFormSubmit}>
-  <input type="text" name="option"/>
-   <button>Hello</button>
- </form>
-</div>
-);
-ReactDOM.render(template, appRoot);
-
-};
-
-render();
+ ReactDOM.render(jsx, document.getElementById('app'));
