1f0b0581df5256227d05f0bf974a363b206b1033
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-04-22 10:39:15 -0500

    remove

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -4,6 +4,7 @@
  		this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
  		this.handlePick = this.handlePick.bind(this);
  		this.handleAddOption = this.handleAddOption.bind(this);
+        this.handleDeleteOption = this.handleDeleteOption.bind(this);
  		this.state = {
  			options: props.options
  		};
@@ -11,6 +12,14 @@
 
  	handleDeleteOptions() {
  		this.setState(() => ({ options: [] }));
+        }
+
+        handleDeleteOption(optionToRemove) {
+            this.setState((prevState) => ({
+                options: prevState.options.filter((option) => optionToRemove !== option)
+                    
+                
+            }));
         }
  	handlePick() {
  		alert('test');
@@ -47,6 +56,7 @@
           <Options 
  		  options={this.state.options}
  		  handleDeleteOptions={this.handleDeleteOptions} 
+          handleDeleteOption={this.handleDeleteOption}
  		  />
  		  <AddOption
  		  handleAddOption={this.handleAddOption} 
@@ -110,20 +120,32 @@ Header.defaultProps = {
  		 <div>
  		  <button onClick={props.handleDeleteOptions}>Remove All</button>
  		 {
- 		  props.options.map((option) => <Option key={option} optionText={option} />)
+ 		  props.options.map((option) => (
+           <Option 
+           key={option} 
+           optionText={option} 
+           handleDeleteOption={props.handleDeleteOption}
+           />
+          ))
  		 }
  		 </div>
 
  		);
- };
+      };
 
  const Option = (props) => {
  	return (
  		<div>
  		 {props.optionText}
+         <button onClick={(e) => {
+            props.handleDeleteOption(props.optionText);
+         }}
+         >
+         remove
+         </button>
  		</div>
  		);
- };
+      };
 
  
 
